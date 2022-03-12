f921cf6493c6873a7603fe3e15752267f02d01ba
heyjochen <94847959+heyjochen@users.noreply.github.com>
2022-03-11 21:47:10 -0600

    Setup index structure
    Including Header, Top Section, Footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        circular: ["Circular"],
+      },
+      colors: {
+        'grayer': '#25282c',
+        'whiter': '#e6e5e4',
+        'reder': '#e54734',
+        'blueer': '#09acf0',
+      },
+    },
   },
   plugins: [],
 }
