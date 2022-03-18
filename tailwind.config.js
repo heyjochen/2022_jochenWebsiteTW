3c8ab925a924a26d38656a6aa0e7eb610777411f
heyjochen <jochen@stierberger.com>
2022-03-17 21:47:20 -0500

    added assets, sites and structures

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,11 @@ module.exports = {
         'whiter': '#f7f2ed',
         'reder': '#f14f3d',
         'blueer': '#79a8c6',
+        
+        'whitealice': '#ECF5FD',
+        'bluejeans': '#63ADF2',
+        'grayjeans': '#304D6D',
+        'blueprussian': '#1F3247',
       },
       height: {
         '128': '32rem',
